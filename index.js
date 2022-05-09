65149505a38f2ceb0d58cffe468c018c08c142ab
Shruti Balasa <shrutibalasawebdev@gmail.com>
2022-05-09 16:02:12 +0530

    Version 1 complete

---
+++ index.js
@@ -0,0 +1,24 @@
+const plugin = require('tailwindcss/plugin')
+const gridAutoFit = plugin(function({ matchUtilities, theme }) {
+   matchUtilities(
+     {
+       'grid-auto-fit': (value) => ({
+         gridTemplateColumns: `repeat(auto-fit, minmax(${value}, 1fr))`
+       }),
+     },
+     { values: theme('gridAutoFit') }
+   )
+ },
+   {
+     theme: {
+      gridAutoFit: {
+         'xs': '12rem',
+         'sm': '14rem',
+         'md': '16rem',
+         'lg': '18rem',
+         'xl': '20rem',
+       },
+     },
+   },
+ )
+ module.exports = gridAutoFit
